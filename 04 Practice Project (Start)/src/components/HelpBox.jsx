ac845c43009cc1d1cb656a415636d14f14975ae3
budumuru-rohit <129642639+budumuru-rohit@users.noreply.github.com>
2024-07-27 19:35:18 +0530

    Update HelpBox.jsx

--- 04 Practice Project (Start)/src/components/HelpBox.jsx
+++ 04 Practice Project (Start)/src/components/HelpBox.jsx
@@ -5,7 +5,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      {/* This is a comment in JSX */}
+      {/* This is a comment in JSX-again */}
       <h2>{title}</h2>
       <p>{text}</p>
     </article>
